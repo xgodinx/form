b66bf33a720ad4e23aab0a772312361bfc0cce33
Dima <96godin@gmail.com>
2025-10-19 15:23:47 +0300

    MUI

---
+++ form/src/components/Burger.jsx
@@ -0,0 +1,100 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import ClickAwayListener from "@mui/material/ClickAwayListener";
+import Grow from "@mui/material/Grow";
+import Paper from "@mui/material/Paper";
+import Popper from "@mui/material/Popper";
+import MenuList from "@mui/material/MenuList";
+import Stack from "@mui/material/Stack";
+import { NavLink } from "react-router";
+
+export default function MenuListComposition() {
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  function handleListKeyDown(event) {
+    if (event.key === "Tab") {
+      event.preventDefault();
+      setOpen(false);
+    } else if (event.key === "Escape") {
+      setOpen(false);
+    }
+  }
+
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
+
+  return (
+    <Stack direction="row" spacing={2}>
+      <div>
+        <Button
+          ref={anchorRef}
+          id="composition-button"
+          aria-controls={open ? "composition-menu" : undefined}
+          aria-expanded={open ? "true" : undefined}
+          aria-haspopup="true"
+          onClick={handleToggle}
+        >
+          <div className="flex flex-col justify-between w-8 h-6">
+            <span className="block h-[3px] w-full rounded bg-gray-800"></span>
+            <span className="block h-[3px] w-full rounded bg-gray-800"></span>
+            <span className="block h-[3px] w-full rounded bg-gray-800"></span>
+          </div>
+        </Button>
+        <Popper
+          open={open}
+          anchorEl={anchorRef.current}
+          role={undefined}
+          placement="bottom-start"
+          transition
+          disablePortal
+        >
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              style={{
+                transformOrigin:
+                  placement === "bottom-start" ? "left top" : "left bottom",
+              }}
+            >
+              <Paper>
+                <ClickAwayListener onClickAway={handleClose}>
+                  <MenuList
+                    autoFocusItem={open}
+                    id="composition-menu"
+                    aria-labelledby="composition-button"
+                    onKeyDown={handleListKeyDown}
+                  >
+                    <div className="flex flex-col py-1 w-[100px] items-center gap-[10px]">
+                      <NavLink to="/">Form</NavLink>
+                      <NavLink to="posts">Posts</NavLink>
+                    </div>
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
+      </div>
+    </Stack>
+  );
+}
--- form/src/components/Header.jsx
+++ form/src/components/Header.jsx
@@ -1,29 +1,30 @@
 import React from "react";
 import { NavLink } from "react-router";
-
+import MenuListComposition from "./Burger";
+import useWindowWidth from "../hooks/useWindowWidth";
 export default function Header() {
+  const width = useWindowWidth();
+  console.log(width);
   return (
-    <div className="bg-blue-400 h-[70px] mb-[50px] w-full p-2.5">
-      <div className="flex justify-between items-center">
-        <NavLink
-          to="/"
-          className=" w-[50px] h-[50px] rounded-full bg-amber-50"
-        ></NavLink>
-        <nav className="flex gap-4">
-          <NavLink
-            to="/"
-            className=" h-8 text-[20px] hover:border-b-2 border-amber-500 font-medium"
-          >
-            form
-          </NavLink>
-          <NavLink
-            to="posts"
-            className="text-[20px] hover:border-b-2 border-amber-500 font-medium"
-          >
-            posts
-          </NavLink>
-        </nav>
-      </div>
+    <div className="bg-blue-400 h-[70px] mb-[50px] w-full flex items-center justify-between p-3.5 ">
+      <NavLink
+        to="/"
+        className=" w-[50px] h-[50px] rounded-full bg-amber-50"
+      ></NavLink>
+      <nav className="flex flex-col ">
+        {width < 768 ? (
+          <MenuListComposition />
+        ) : (
+          <div className="flex gap-[20px]">
+            <NavLink to="/" className=" text-[20px]  font-medium">
+              form
+            </NavLink>
+            <NavLink to="posts" className="text-[20px] font-medium">
+              posts
+            </NavLink>
+          </div>
+        )}
+      </nav>
     </div>
   );
 }
